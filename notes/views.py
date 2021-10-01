177148630f999ba4c5e57c24b15e4a4ac2665dcd
Vishnu <vishnuak15@gmail.com>
2021-10-02 00:08:59 +0530

    custom error page 404

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
 from .models import Notes
@@ -6,3 +7,10 @@ from .models import Notes
 def list(request):
     all_notes = Notes.objects.all()
     return render(request,'notes/notes_list.html',{'notes':all_notes})
+
+def detail(request,pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Notes does'nt exist")
+    return render(request, 'notes/notes_detial.html',{'notes':note})
