96a623dd14ec6d02de882b096dda81b7f294bcf7
Vishnu <vishnuak15@gmail.com>
2021-10-02 23:31:23 +0530

    Class based views

--- notes/views.py
+++ notes/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request,'notes/notes_list.html',{'notes':all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_detial.html"
+
 
 def detail(request,pk):
     try:
