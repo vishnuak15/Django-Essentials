2a5c238cba254c81cab46e8a4e2d4c79d8378404
Vishnu <vishnuak15@gmail.com>
2021-10-04 23:48:08 +0530

    Styling

--- home/views.py
+++ home/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
-    extra_content = {'today': datetime.today()}
+    extra_content = {'today':datetime.now()}
 
 class AuthorizedView(LoginRequiredMixin,TemplateView):
     template_name = 'home/authorized.html'
