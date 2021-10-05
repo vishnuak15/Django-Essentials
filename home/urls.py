77f5d6fb4ef1d112e31858e0dd40c73447d09423
Vishnu <vishnuak15@gmail.com>
2021-10-06 00:35:09 +0530

    SignUp done

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home',views.HomeView.as_view()),
+    path('',views.HomeView.as_view()),
     path('authorized', views.AuthorizedView.as_view()),
     path('login', views.LoginInterfaceView.as_view(),name='login'),
     path('logout', views.LogoutInterfaceView.as_view()),
+    path('signup', views.SignupView.as_view(),name='signup'),
 ]
