4ab2322bc02d1f35f326fc47d83bcbce85282ba3
Vishnu <vishnuak15@gmail.com>
2021-10-05 23:50:26 +0530

    save the Form

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Notes(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)# automatically adds the current date to the database
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
--- notes/views.py
+++ notes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import HttpResponseRedirect
 
 # Create your views here.
 from .models import Notes
@@ -16,10 +18,22 @@ class NotesCreateView(CreateView):
     model = Notes
     form_class = NotesForm
     success_url = '/smart/notes'
-class NotesListView(ListView):
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin,ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
 
 class NotesUpdateView(UpdateView):
     model = Notes
