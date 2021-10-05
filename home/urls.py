d3e70dd6b43b556b05ef5d27d6e956c51064b50a
Vishnu <vishnuak15@gmail.com>
2021-10-06 00:19:47 +0530

    Login done

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('home',views.HomeView.as_view()),
     path('authorized', views.AuthorizedView.as_view()),
+    path('login', views.LoginInterfaceView.as_view(),name='login'),
+    path('logout', views.LogoutInterfaceView.as_view()),
 ]
